3b2ff32d0e525784d6c41a7949b3be0b81f1a5b9
Md. Sajedul Islam <sajedulislms@gmail.com>
2023-09-01 17:15:43 +0600

    checkout page on progress (date)

--- client/app/hotels/[hotelId]/checkout/page.js
+++ client/app/hotels/[hotelId]/checkout/page.js
@@ -1,18 +1,28 @@
 'use client'
 import { useGetRoomByIdQuery } from '@/lib/redux/features/room/roomApi'
-import React from 'react'
+import React, { useState } from 'react'
 import { useSearchParams } from 'next/navigation'
 import { BsCalendarEvent } from 'react-icons/bs'
 import { TbStairs } from 'react-icons/tb'
 import { MdPeopleOutline } from 'react-icons/md'
 import {LuBath, LuBedDouble} from 'react-icons/lu'
-import Button from '@/components/buttons/Button'
 import { FaSmoking, FaSmokingBan } from 'react-icons/fa'
+import { useForm } from 'react-hook-form'
+import InputField from '@/components/inputs/InputField'
+import DatePicker from "react-datepicker";
+import {format,addDays,differenceInDays } from 'date-fns'
+import "react-datepicker/dist/react-datepicker.css";
 
 
 export default function Checkout() {
   const searchParams = useSearchParams()
   const roomId = searchParams.get('room')
+  const { register,  handleSubmit, watch, formState: { errors } } = useForm();
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date(addDays(startDate,3)));
+
+  console.log(startDate,endDate);
+
   
   const {data,isLoading,isError,error} = useGetRoomByIdQuery(roomId);
   
@@ -76,6 +86,97 @@ export default function Checkout() {
                 </div>
               </div>
             </div>
+            <div className='pt-8'>
+              <h1>Overview</h1>
+
+            </div>
+            <div className='pt-8'>
+              <h2>Enter your details</h2>
+              <div>
+                <form action="">
+                  <div className="grid md:grid-cols-2 grid-cols-1 gap-3">
+                      <div>
+                        <InputField 
+                              label={"Your name"}
+                              id={'name'}
+                              type={'text'}
+                              placeholder={'Your name'}
+                              name={'name'}
+                              register={register}
+                              required="Name is required"
+                              minLength={8}
+                          />
+                          {errors.name && <span className='text-red-600 text-xs py-3'>{errors.name.message}</span>}
+                      </div>
+                      <div>
+                        <InputField 
+                              label="Email adddress"
+                              id={'email'}
+                              type={'email'}
+                              placeholder={' Your Email'}
+                              name={'email'}
+                              register={register}
+                              required="Email is required"
+                            pattern={{
+                              value:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                              message:"Invalid email address"
+                            }}
+                          />
+                          {errors.email && <span className='text-red-600 text-xs py-3'>{errors.email.message}</span>}
+                      </div>
+                      <div>
+                        <InputField 
+                              label={"Your phone"}
+                              id={'phone'}
+                              type={'number'}
+                              placeholder={'Your phone'}
+                              name={'name'}
+                              register={register}
+                              required="Phone number is required"
+                              minLength={11}
+                          />
+                          {errors.phone && <span className='text-red-600 text-xs py-3'>{errors.phone.message}</span>}
+                      </div>
+                      <div>
+                        <InputField 
+                              label={"Guests"}
+                              id={'guests'}
+                              type={'number'}
+                              placeholder={'Guests'}
+                              name={'guests'}
+                              register={register}
+                              required="Guests number is required"
+                              minLength={1}
+                          />
+                          {errors.guests && <span className='text-red-600 text-xs py-3'>{errors.guests.message}</span>}
+                      </div>
+                      <div>
+                        <label className='text-sm' htmlFor="checkInDate">Check In</label> <br />
+                        <DatePicker
+                          selected={startDate}
+                          onChange={(date) => setStartDate(date)}
+                          selectsStart
+                          startDate={startDate}
+                          endDate={endDate}
+                          className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mt-2'
+                        />
+                      </div> 
+                      <div>
+                      <label className='text-sm' htmlFor="checkInDate">Check out</label> <br />
+                      <DatePicker
+                        selected={endDate}
+                        onChange={(date) => setEndDate(date)}
+                        selectsEnd
+                        startDate={startDate}
+                        endDate={endDate}
+                        minDate={startDate}
+                        className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mt-2'
+                      />
+                      </div>
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
           <div className='p-3 w-full'>
             <div className='border rounded p-3 bg-white shadow'>
@@ -84,20 +185,20 @@ export default function Checkout() {
                 <div className="flex justify-between px-2 text-xs pb-2">
                   <div className=''>
                     <p className='pb-2 text-gray-500'>CHECK-IN</p>
-                    <p className='text-sm font-semibold'>Sun, 22 May 2023</p>
+                    <p className='text-sm font-semibold'>{format(new Date(startDate),'ccc, d LLL uu')}</p>
                     <p>From {data?.hotel?.checkInTime}</p>
                   </div>
                   <div className='border-r-2'></div>
                   <div>
                     <p className='pb-2 text-gray-500'>CHECK-OUT</p>
-                    <p className='text-sm font-semibold'>Wed, 25 May 2023</p>
+                    <p className='text-sm font-semibold'>{format(new Date(endDate),'ccc, d LLL uu')}</p>
                     <p>From {data?.hotel?.checkOutTime}</p>
                   </div>
                 </div>
                 <div className='p-2'>
                   <p className='text-gray-500 text-xs'>TOTAL LENGTH OF STAY</p>
                   <div className='flex items-center'>
-                    <span className='text-gray-500 text-xs pe-2'>3 days </span> <BsCalendarEvent color='gray' fontSize={'14px'}/>
+                    <span className='text-gray-500 text-xs pe-2'>{differenceInDays(endDate,startDate)} days </span> <BsCalendarEvent color='gray' fontSize={'14px'}/>
                   </div>
                 </div>
               </div>
