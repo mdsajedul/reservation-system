09baf37b92c21a92d14f1bf7902ab3ea92b6b6d9
mdsajedul <sajedulislms@gmail.com>
2024-01-02 16:48:25 +0600

    vercel json added

--- server/app/routes.js
+++ server/app/routes.js
@@ -6,7 +6,23 @@ router.get('/health',(_req,res)=>{
     res.status(200).json({message:'Success'})
 })
 router.get('/',(req,res)=>{
-    res.status(200).render('../index.html')
+    res.status(200).send(`
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Reservation</title>
+        </head>
+        <body>
+            <div style="display: flex;justify-content: center;">
+                <div>
+                    <h3>Welcome to Reservation!!!</h3>
+                </div>
+            </div>
+        </body>
+    </html>
+    `)
 })
 
 module.exports = router
