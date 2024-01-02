1c39fac1cb3020a3e8797d8d1e8474e3d5e5df79
mdsajedul <sajedulislms@gmail.com>
2024-01-02 16:13:06 +0600

    added html page

--- server/app/routes.js
+++ server/app/routes.js
@@ -5,5 +5,8 @@ router.use(require('../routes/index'))
 router.get('/health',(_req,res)=>{
     res.status(200).json({message:'Success'})
 })
+router.get('/',(req,res)=>{
+    res.status(200).render('../index.html')
+})
 
 module.exports = router
